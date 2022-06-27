8807066e2cc25ed12a1fc99c18b24f4be704f36a
EddieHozan <45522019+EddieHozan@users.noreply.github.com>
2022-06-27 11:10:54 -0400

    Formatting

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-  cout << "Fibonacci Sequence Program by Eddie Hozan\n";
+  	cout << "Fibonacci Sequence Program by Eddie Hozan\n";
 	int terms;
 	int t1 = 0;
 	int t2 = 1; 
 	int result;
 	cout << "Enter the number of terms to print: ";
-  cin >> terms;
+  	cin >> terms;
 	
 	cout << t1 + t2 << ",";
 	for (int x = 1; x <= terms; x++){
