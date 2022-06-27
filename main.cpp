d225584a5d25ac8f2ffd76f29e583f231a1ba3b9
Mark Hozan <92109013+MarkHozan@users.noreply.github.com>
2022-06-27 11:01:20 -0400

    It's been a few years...
    Redid this code, very error prone haha. Also tried out Vim

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 int main() {
-  int t1 = 0, t2 = 1, uterm = 0;
-  int terms;
-  cout << "Enter the number of terms to print: ";
-  cin >> terms;
-
-  uterm = t1 + t2;
   cout << "Fibonacci Sequence Program by Eddie Hozan\n";
-  cout << t1 << " , " << t2 << " , ";
+	int terms;
+	int t1 = 0;
+	int t2 = 1; 
+	int result;
+	cout << "Enter the number of terms to print: ";
+  cin >> terms;
+	
+	cout << t1 + t2 << ",";
+	for (int x = 1; x <= terms; x++){
+		result = t1 + t2;
+		cout << result << ",";
+		t1 = t2;		
+		t2 = result;
+	};
+	
 
-  for(int x = 0;x<terms-2;x++){
-    cout << uterm << " , ";
-      t1 = t2;
-      t2 = uterm;
-      uterm = t1 + t2;
-  }
-  return 0;
+  	return 0;
 }
+
